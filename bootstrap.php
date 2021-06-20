364874d6fb1a422e96dc222d94b0eaf0d21d2c28
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-06-20 15:47:57 +0300

    Initial commit

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+// define('URL', 'http://localhost/php_projects/bankasOop/');
+define('INSTALL_DIR','/cli/vakcina/');
+define('DIR', __DIR__.'/');
+// require DIR.'app/functions.php';
+// require DIR.'app/BankController.php';
+require DIR.'Json.php';
+require DIR.'User.php';
+require DIR.'Validation.php';
+require DIR.'InputReader.php';
+
+_d($_SESSION, 'SESIJA--->');
+?>
