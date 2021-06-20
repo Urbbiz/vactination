cf5235f3bfa8693553802940353d16c334f0e099
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-06-20 16:15:28 +0300

    added existing usser

--- index.php
+++ index.php
@@ -2,6 +2,43 @@
 
 
 
+// ************************************** EXISTING  USER  ****************************************************************
+if ($foundUser != null){   // jeigu toks id jau yra sistemoje
+
+    Echo "\n", "Welcome back ", $foundUser->name, " If you want to  edit appointment time enter:1, If you want to delete appointment enter:2 : ";
+    $isInputValid = false;
+    do {
+        $input = trim(fgets(STDIN, 1024));
+
+        if ($input == '1') {
+            $isInputValid = true;
+            echo "Enter new time: ";
+
+            $dateTime = InputReader::readDateTime();
+            
+            
+            $foundUser->dateTime =$dateTime;
+
+            Json::getDB()->updateAdd($foundUser); 
+            echo "Your new time is changed!";
+
+           
+        } 
+        elseif ($input == '2') {
+            $isInputValid = true;
+           
+            echo "your appointment date and time deleted";
+            $foundUser->dateTime = null;
+         
+            Json::getDB()->updateAdd($foundUser);
+
+        } else  {
+            echo "Your input was inncorect, please try again! ";
+
+        }
+    }while ($isInputValid == false);
+}
+
 // **************************************  NEW USER  *********************************************************************
 else {   // jeigu id nera sistemoje
 
